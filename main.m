68aa246e90979d5fd3ad3141762dfa2a65f291aa
ofir kedem <ofirk101@gmail.com>
2023-02-23 18:33:40 +0200

    some changes

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ addpath('tensorlab_2016-03-28')
 
 % Load the image
 path = 'images/dancer.jpg';
-im_size = 50;
+im_size = 100;
 
 img = imread(path);
 img = imresize(img, [im_size, im_size]);
@@ -11,20 +11,25 @@ img_gray = rgb2gray(img);
 x = im2double(img_gray);
 
 n = size(x,1);
-m = 2 * n -1;
+m = (2 * n);
 
-y = abs(fft2(zero_pad(x, m))) .^ 2;
-z_0 = ifft2(sqrt(y) .* exp(1j * 2 * pi * randn(m)));
+y = abs(fftshift(fft2(zero_pad(x, m))) ./ m) .^ 2;
+z_0 = ifftshift(ifft2(sqrt(y) .* exp(1j * 2 * pi * randn(m)))) .* m;
+
+
 gamma = ones(1, m^2 - n^2);
-r = 1;
+scale =  0.1;
+global r
+r = rand() * scale;
 
-f = @(z)G(z,y,n,r, gamma);
-g = @(z)grad(z, y, n, r, gamma);
+f = @(z)G(z,y,n, gamma);
+g = @(z)grad(z, y, n, gamma);
 
 tmp = f(z_0);
 tmp_g = g(z_0);
 
 [z,output] = minf_lbfgs(f,g,z_0);
+f_final = f(z);
 x_hat = abs(z(1:n, 1:n));
 
 imshow(x_hat)
@@ -39,8 +44,9 @@ end
 
 
 function res = proj_m(z,y)
-    fft_z = fft2(z);
-    res = ifft2(sqrt(y) .* fft_z ./ abs(fft_z));
+    m = size(y,1);
+    fft_z = fftshift(fft2(z)) ./ m;
+    res = ifftshift(ifft2(sqrt(y) .* fft_z ./ abs(fft_z))) .* m;
 end
 
 function res = a(z, n)
@@ -60,16 +66,19 @@ function res = a_star(gamma, n)
     res(ind2sub([m,m], idxs)) = conj(gamma);
 end
 
-function objective = G(z, y, n, r, gamma)
+function objective = G(z, y, n, gamma)
+    global r
+%     r = rand() * 0.1;
     proj_m_z = proj_m(z, y);
     z_minus_proj_m_z = z - proj_m_z;
     a_z = a(z, n);
-    objective = 0.5 * norm(z_minus_proj_m_z, 'fro')^2 + (r/2) * norm(a_z + gamma)^2; 
-    
+    objective = 0.5 * norm(z_minus_proj_m_z, 'fro')^2 + (r/2) * norm(a_z + gamma)^2;   
 end
 
 
-function res = grad(z, y, n, r, gamma)
+function res = grad(z, y, n, gamma)
+    global r
+    disp(r)
     proj_m_z = proj_m(z, y);
     z_minus_proj_m_z = z - proj_m_z;
     a_z = a(z, n);
